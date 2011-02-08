ec6828fe84277d05d5447f9658beeb3915a8ed30
alan <alan@localhost.(none)>
2011-02-08 01:13:27 +0000

    Blow out xls stuff, bring in stringtemplate and supercsv

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject boat "0.0.1-SNAPSHOT"
   :description "Some shit that does stuff with boats."
+  :repositories {"intalio" "http://www.intalio.org/public/maven2/"}
   :dependencies [[org.clojure/clojure "1.3.0-alpha4"]
-                 ;; TODO pare down to only the libraries we need
                  [org.clojure.contrib/standalone "1.3.0-alpha4"]
-                 [jexcelapi/jxl "2.4.2"]]
+                 [org.supercsv/SuperCSV "1.52"]
+                 [org.antlr/stringtemplate "3.2"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
   :main boat.core)
--- src/boat/core.clj
+++ src/boat/core.clj
@@ -1,28 +1,5 @@
 (ns boat.core
-  (:use [clojure.java.io :only (file)])
-  (:import (jxl Workbook Sheet Cell))
   (:gen-class))
-
-(defn ^Sheet sheet-from-file
-  "Returns the nth sheet from File or filename f.
-   When provided f only, return the first (0th) sheet."
-  ([f n]
-     (.getSheet (Workbook/getWorkbook (file f)) n))
-  ([f]
-     (grab-sheet f 0)))
-
-(defn get-row
-  "Given sheet, row number n, and the column to start at,
-   returns a sequence of non-empty cell values.
-
-   Given only sheet and row number n, returns the rows starting at
-   column 0. "
-  ([^Sheet sheet n start]
-     (->> (iterate inc start)
-          (map #(.. sheet (getCell % n) getContents))
-          (take-while (partial not= ""))))
-  ([sheet n]
-     (get-row sheet n 0)))
 
 (defn -main [& args]
   (println "Yo, boat here!"))
